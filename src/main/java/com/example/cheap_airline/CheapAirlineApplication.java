535e7817430b8692d2beb717000d0c0a78f58d70
BulliedDog <albertorovai1@gmail.com>
2025-11-06 13:43:34 +0100

    Made better suited folder structure for Spring to work

---
+++ src/main/java/com/example/cheap_airline/CheapAirlineApplication.java
@@ -0,0 +1,12 @@
+package com.example.cheap_airline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheapAirlineApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CheapAirlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cheap_airline/CheapAirlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cheap_airline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheapAirlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
