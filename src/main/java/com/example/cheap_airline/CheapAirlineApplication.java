0785edef9fb1c676ccfa7e3296ef87e3cc1339c2
BulliedDog <albertorovai1@gmail.com>
2026-01-28 12:54:43 +0100

    Made better suited folder structure for Spring to work

--- src/main/java/com/example/cheap_airline/CheapAirlineApplication.java
+++ src/main/java/com/example/cheap_airline/CheapAirlineApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CheapAirlineApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(CheapAirlineApplication.class, args);
+        SpringApplication.run(CheapAirlineApplication.class, args);
 	}
-
 }
